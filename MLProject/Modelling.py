ebf69db3c0ac6600bf4d06e815c946dbbf7aa1d0
Yoga Permana Sukma <yogapermanasukma102@gmail.com>
2025-06-14 13:05:58 +0700

    Fixing Path of Data

--- MLProject/Modelling.py
+++ MLProject/Modelling.py
@@ -27,10 +27,10 @@ mlflow.set_tracking_uri("https://dagshub.com/YogaPermanaSukma1008/membangun-mode
 mlflow.sklearn.autolog(log_models=True)
 
 # ========== 2. Load Data ==========
-X_train = pd.read_csv("loandata_preprocessing/X_train_processed.csv")
-X_test = pd.read_csv("loandata_preprocessing/X_test_processed.csv")
-y_train = pd.read_csv("loandata_preprocessing/y_train.csv")
-y_test = pd.read_csv("loandata_preprocessing/y_test.csv")
+X_train = pd.read_csv("MLProject/loandata_preprocessing/X_train_processed.csv")
+X_test = pd.read_csv("MLProject/loandata_preprocessing/X_test_processed.csv")
+y_train = pd.read_csv("MLProject/loandata_preprocessing/y_train.csv")
+y_test = pd.read_csv("MLProject/loandata_preprocessing/y_test.csv")
 
 y_train = y_train.values.ravel()
 y_test = y_test.values.ravel()
