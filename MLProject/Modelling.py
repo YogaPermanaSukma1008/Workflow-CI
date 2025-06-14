4dbbd0cda42ff8f8dfd81a7728d5df67c7a991a6
Yoga Permana Sukma <yogapermanasukma102@gmail.com>
2025-06-14 21:15:58 +0700

    Revisi File

--- MLProject/Modelling.py
+++ MLProject/Modelling.py
@@ -32,10 +32,10 @@ mlflow.sklearn.autolog(log_models=False)
 
 # ========== 2. Load Data ==========
 try:
-    X_train = pd.read_csv("MLProject/loandata_preprocessing/X_train_processed.csv")
-    X_test = pd.read_csv("MLProject/loandata_preprocessing/X_test_processed.csv")
-    y_train = pd.read_csv("MLProject/loandata_preprocessing/y_train.csv")
-    y_test = pd.read_csv("MLProject/loandata_preprocessing/y_test.csv")
+    X_train = pd.read_csv("loandata_preprocessing/X_train_processed.csv")
+    X_test = pd.read_csv("loandata_preprocessing/X_test_processed.csv")
+    y_train = pd.read_csv("loandata_preprocessing/y_train.csv")
+    y_test = pd.read_csv("loandata_preprocessing/y_test.csv")
 
     y_train = y_train.values.ravel()
     y_test = y_test.values.ravel()
